e253f6620b6b9b8fb96df0777f888236883b3b86
Randy Maulana <randy8@mhs.usk.ac.id>
2025-08-16 07:47:23 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const charList = document.getElementById("char-list");    
+    
+    fetch("data-karakter.json")
+      .then(res => res.json())
+      .then(data => {
+        const list = document.getElementById("char-list");
+        data.forEach(char => {
+          const card = document.createElement("a");
+          card.href = `profil.html?id=${char.id}`;
+          card.className = "card";
+          card.innerHTML = `
+            <img src="${char.thumbnail}" alt="${char.nama}">
+            <h3>${char.nama}</h3>
+            <p>${char.anime}</p>
+          `;
+          list.appendChild(card);
+        });
+      })
+      .catch(err => {
+        console.error("Gagal memuat karakter:", err);
+      });
+
+
+  
+
+
+  
+})
